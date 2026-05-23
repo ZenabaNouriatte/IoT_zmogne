4210158a4a5cf268baa0e226a6198a006bb4a61a
Nouriatte <zenaba.mogne@live.fr>
2026-05-23 17:51:36 +0200

    script ok

--- p3/scripts/launch.sh
+++ p3/scripts/launch.sh
@@ -6,7 +6,7 @@ k3d cluster create iotcluster
 
 # Configuration de kubectl pour accéder au cluster
 mkdir -p ~/.kube
-cp $HOME/.config/k3d/kubeconfig-iotcluster.yaml ~/.kube/config
+k3d kubeconfig merge iotcluster --kubeconfig-switch-context
 
 
 # Création des namespaces
@@ -19,4 +19,8 @@ echo "====  ARGO CD INSTALL ====="
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 #  Verifier aue les pods argocd sont en running
+kubectl get pods -n argocd
+
+echo "====  WAITING FOR ARGO CD PODS TO BE READY ====="
+kubectl wait --for=condition=ready pod --all -n argocd --timeout=300s
 kubectl get pods -n argocd
